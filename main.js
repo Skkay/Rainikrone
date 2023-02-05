a9964e2705d9d6140d6dfce90faa014cfb472150
Skkay <skkay.thomas@gmail.com>
2023-02-05 19:01:35 +0100

    feat: Add character and basic moves

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 import './style.css';
-import { init } from './src/game.js';
+import { init, moveCharacterLeft, moveCharacterRight, moveCharacterUp, moveCharacterDown } from './src/game.js';
 
 init();
+
+const moveLeftButtonEl = document.getElementById('moveLeftButton');
+const moveRightButtonEl = document.getElementById('moveRightButton');
+const moveUpButtonEl = document.getElementById('moveUpButton');
+const moveDownButtonEl = document.getElementById('moveDownButton');
+
+moveLeftButtonEl.addEventListener('click', () => moveCharacterLeft());
+moveRightButtonEl.addEventListener('click', () => moveCharacterRight());
+moveUpButtonEl.addEventListener('click', () => moveCharacterUp());
+moveDownButtonEl.addEventListener('click', () => moveCharacterDown());
--- src/game.js
+++ src/game.js
@@ -1,5 +1,18 @@
 const VARIABLE_CELL_TYPES = ['red-cell', 'yellow-cell', 'black-cell', 'green-cell'];
 
+const BOARD_PADDING = 5;
+const GRID_GAP = 2;
+const CELL_WIDTH = 50;
+
+const CHARACTER_WIDTH = 34;
+
+const character = {
+    position: {
+        x: 5,
+        y: 5,
+    },
+};
+
 const init = () => {
     const cells = document.querySelectorAll('.cell:not(.wall):not(.start):not(.pink-cell)');
 
@@ -8,6 +21,36 @@ const init = () => {
 
         element.classList.add(randomCell);
     });
+
+    moveCharacterTo(character.position.x, character.position.y);
 };
 
-export { init };
+const moveCharacterTo = (x, y) => {
+    const left = CELL_WIDTH / 2 - CHARACTER_WIDTH / 2 + BOARD_PADDING + (x * CELL_WIDTH + x * GRID_GAP);
+    const top = CELL_WIDTH / 2 - CHARACTER_WIDTH / 2 + BOARD_PADDING + (y * CELL_WIDTH + y * GRID_GAP);
+
+    const char = document.getElementById('character');
+    char.style.top = top + 'px';
+    char.style.left = left + 'px';
+
+    character.position.x = x;
+    character.position.y = y;
+};
+
+const moveCharacterLeft = () => {
+    moveCharacterTo(character.position.x - 1, character.position.y);
+};
+
+const moveCharacterRight = () => {
+    moveCharacterTo(character.position.x + 1, character.position.y);
+};
+
+const moveCharacterUp = () => {
+    moveCharacterTo(character.position.x, character.position.y - 1);
+};
+
+const moveCharacterDown = () => {
+    moveCharacterTo(character.position.x, character.position.y + 1);
+};
+
+export { init, moveCharacterLeft, moveCharacterRight, moveCharacterUp, moveCharacterDown };
