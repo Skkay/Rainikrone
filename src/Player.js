3ff3c433a01ed4c86900b361d0debcbe2b15ceb5
Skkay <skkay.thomas@gmail.com>
2023-02-11 23:01:21 +0100

    feat(Player): Add previous coords properties

--- src/Player.js
+++ src/Player.js
@@ -6,6 +6,8 @@ const BOARD_PADDING = 5;
 const GRID_GAP = 2;
 
 class Player {
+    previousX;
+    previousY;
     x;
     y;
     playerEl;
@@ -15,15 +17,27 @@ class Player {
         playerEl.setAttribute('id', 'player');
 
         this.playerEl = playerEl;
-        this.moveTo(startX, startY);
+        this.moveTo(startX, startY, startX, startY);
     }
 
-    moveTo(x, y) {
+    moveTo(x, y, previousX = null, previousY = null) {
         const top = CELL_HEIGHT / 2 - PLAYER_HEIGHT / 2 + BOARD_PADDING + (y * CELL_HEIGHT + y * GRID_GAP);
         const left = CELL_WIDTH / 2 - PLAYER_WIDTH / 2 + BOARD_PADDING + (x * CELL_WIDTH + x * GRID_GAP);
 
         this.playerEl.style.top = `${top}px`;
         this.playerEl.style.left = `${left}px`;
+
+        if (previousX !== null) {
+            this.previousX = previousX;
+        } else {
+            this.previousX = this.x;
+        }
+
+        if (previousY !== null) {
+            this.previousY = previousY;
+        } else {
+            this.previousY = this.y;
+        }
 
         this.x = x;
         this.y = y;
