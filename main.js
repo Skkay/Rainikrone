d693d5841924c182cc7c7b7f04cbca40ee330ed5
Skkay <skkay.thomas@gmail.com>
2023-02-17 20:28:56 +0100

    feat: Update game infos on player move

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ const moveRightButton = document.getElementById('moveRightButton');
 const moveUpButton = document.getElementById('moveUpButton');
 const moveDownButton = document.getElementById('moveDownButton');
 
+const remainingObjectivesEl = document.getElementById('remainingObjectives');
+const moveCountEl = document.getElementById('moveCount');
+
 fetch('boards/default.json')
     .then((res) => res.json())
     .then((board) => startGame(board));
@@ -16,20 +19,44 @@ const startGame = (board) => {
     game.bootstrap();
     game.init();
 
-    moveLeftButton.addEventListener('click', () => game.movePlayerLeft());
-    moveRightButton.addEventListener('click', () => game.movePlayerRight());
-    moveUpButton.addEventListener('click', () => game.movePlayerUp());
-    moveDownButton.addEventListener('click', () => game.movePlayerDown());
+    remainingObjectivesEl.innerText = game.remainingObjectives;
+    moveCountEl.innerText = game.moveCount;
+
+    moveLeftButton.addEventListener('click', () => {
+        game.movePlayerLeft();
+        updateGameInfo(game);
+    });
+    moveRightButton.addEventListener('click', () => {
+        game.movePlayerRight();
+        updateGameInfo(game);
+    });
+    moveUpButton.addEventListener('click', () => {
+        game.movePlayerUp();
+        updateGameInfo(game);
+    });
+    moveDownButton.addEventListener('click', () => {
+        game.movePlayerDown();
+        updateGameInfo(game);
+    });
 
     document.addEventListener('keyup', (event) => {
         if (event.key === 'ArrowLeft') {
             game.movePlayerLeft();
+            updateGameInfo(game);
         } else if (event.key === 'ArrowRight') {
             game.movePlayerRight();
+            updateGameInfo(game);
         } else if (event.key === 'ArrowUp') {
             game.movePlayerUp();
+            updateGameInfo(game);
         } else if (event.key === 'ArrowDown') {
             game.movePlayerDown();
+            updateGameInfo(game);
         }
     });
+};
+
+const updateGameInfo = (game) => {
+    remainingObjectivesEl.innerText = game.remainingObjectives;
+    moveCountEl.innerText = game.moveCount;
 };
